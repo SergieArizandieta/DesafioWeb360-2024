5b796737b52daaf947ceafb950d614d0bfdffd80
Sergie Arizandieta <78749398+SergieArizandieta@users.noreply.github.com>
2024-12-04 23:49:33 -0600

    Update tables.sql

--- SQL/SQLServer/tables.sql
+++ SQL/SQLServer/tables.sql
@@ -1,11 +1,8 @@
--- EXEC sp_helpindex 'Rol';
-
 
 CREATE TABLE Rol (
    id_rol INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
    name VARCHAR(50) NOT NULL
 );
-
 
 CREATE TABLE Status (
    id_status INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
@@ -19,6 +16,7 @@ CREATE TABLE Category(
    status_id_status INT NOT NULL,
 );
 
+-- add the FK: Category - Status relationship
 ALTER TABLE Category 
 ADD CONSTRAINT FK_Category_Status 
 FOREIGN KEY (status_id_status) REFERENCES Status(id_status);
@@ -35,14 +33,28 @@ CREATE TABLE [User](
    rol_id_rol INT NOT NULL,
 );
 
+-- add phone format check constraint
+ALTER TABLE [User]
+ADD CONSTRAINT chk_phone_format CHECK (phone LIKE '[1-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]');
+
+-- add unique constraint for email
+ALTER TABLE [User]
+ADD CONSTRAINT uq_user_email UNIQUE (email);
+
+-- add check constraint for birth date to be at least 18 years ago
+ALTER TABLE [User]
+ADD CONSTRAINT chk_birth_date_18_years
+CHECK (DATEDIFF(YEAR, birth_date, GETDATE()) >= 18);
+
+-- add FK: User - User and Status relationships
 ALTER TABLE [User]
 ADD CONSTRAINT FK_User_Status
 FOREIGN KEY (status_id_status) REFERENCES Status(id_status);
 
+-- add FK: User - Rol relationship
 ALTER TABLE [User]
 ADD CONSTRAINT FK_User_Rol
 FOREIGN KEY (rol_id_rol) REFERENCES Rol(id_rol);
-
 
 CREATE TABLE Client(
    id_clientDPI CHAR(13) NOT NULL PRIMARY KEY,
@@ -52,6 +64,7 @@ CREATE TABLE Client(
    address VARCHAR(255) NOT NULL,
 );
 
+-- add FK: Client - User relationship
 ALTER TABLE Client
 ADD CONSTRAINT FK_Client_User
 FOREIGN KEY (id_clientDPI) REFERENCES [User](id_userDPI);
@@ -69,10 +82,17 @@ CREATE TABLE Product(
    status_id_status INT NOT NULL,
 );
 
+-- add check constraint for price and stock to be non-negative
+ALTER TABLE Product
+ADD CONSTRAINT chk_price_and_stock_non_negative
+CHECK (price >= 0 AND stock >= 0);
+
+-- add FK: Product - Category and Status relationships
 ALTER TABLE Product
 ADD CONSTRAINT FK_Product_Category
 FOREIGN KEY (category_id_category) REFERENCES Category(id_category);
 
+-- add FK: Product - Status relationship
 ALTER TABLE Product
 ADD CONSTRAINT FK_Product_Status
 FOREIGN KEY (status_id_status) REFERENCES Status(id_status);
@@ -87,10 +107,22 @@ CREATE TABLE [Order](
    user_id_user CHAR(13) NOT NULL,
 );
 
+-- add check constraints for total being non-negative
+ALTER TABLE [Order]
+ADD CONSTRAINT chk_total_non_negative
+CHECK (total >= 0);
+
+-- add check constraint for delivery date to be after creation date
+ALTER TABLE [Order]
+ADD CONSTRAINT chk_delivery_date_valid 
+CHECK (delivery_date >= CAST(creation_date AS DATE));
+
+-- add FK: Order - Client and User relationships
 ALTER TABLE [Order]
 ADD CONSTRAINT FK_Order_Client
 FOREIGN KEY (client_id_client) REFERENCES Client(id_clientDPI);
 
+-- add FK: Order - User relationship
 ALTER TABLE [Order]
 ADD CONSTRAINT FK_Order_User
 FOREIGN KEY (user_id_user) REFERENCES [User](id_userDPI);
@@ -103,10 +135,19 @@ CREATE TABLE Detail(
    order_id_order INT NOT NULL,
 );
 
+-- add check constraints for quantity and subtotal being non-negative
+ALTER TABLE Detail
+ADD CONSTRAINT chk_quantity_and_subtotal_non_negative
+CHECK (quantity > 0 and subtotal >= 0);
+
+-- add FK: Detail - Product and Order relationships
 ALTER TABLE Detail
 ADD CONSTRAINT FK_Detail_Product
 FOREIGN KEY (product_id_product) REFERENCES Product(id_product);
 
+-- add FK: Detail - Order relationship
 ALTER TABLE Detail
 ADD CONSTRAINT FK_Detail_Order
 FOREIGN KEY (order_id_order) REFERENCES [Order](id_order);
+
+-- EXEC sp_helpindex 'Rol';
